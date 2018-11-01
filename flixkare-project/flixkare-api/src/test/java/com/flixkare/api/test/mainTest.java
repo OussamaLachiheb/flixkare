a3f598526efc0940ffa09be5b8015137410ffbac
Oussama Lachiheb <oussama.lachiheb@gmail.com>
2018-11-01 21:12:00 +0100

    Add test folder and config folder

---
+++ flixkare-project/flixkare-api/src/test/java/com/flixkare/api/test/mainTest.java
@@ -0,0 +1,13 @@
+package com.flixkare.api.test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+
+public class mainTest {
+
+@Test
+public void justFirstTest() {
+	assertThat("ouss").isEqualTo("ouss");
+}
+}
