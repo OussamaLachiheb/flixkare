a4c72f5479fa6753d01c5735259ede330f95af0c
Oussama Lachiheb <oussama.lachiheb@gmail.com>
2019-05-01 19:29:48 +0200

    Introduce Swagger in the application for API doc

--- flixkare-project/flixkare-api/src/main/java/com/flixkare/api/controller/testController.java
+++ flixkare-project/flixkare-api/src/main/java/com/flixkare/api/controller/testController.java
@@ -1,12 +1,14 @@
 package com.flixkare.api.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class testController {
 
-	@RequestMapping("/greeting")
+	@RequestMapping(value="/greeting",method = RequestMethod.GET)
+	
 	public String greeting() {
 		return "bonjour oussama";
 	}
